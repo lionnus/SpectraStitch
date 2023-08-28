5ad42ab779e418aa1d5cb8f39563fa739f97516d
lkesting <lkesting@student.ethz.ch>
2023-08-28 02:02:04 +0200

    Seperated upload, live tweaking of output image, added text

--- webapp/app.py
+++ webapp/app.py
@@ -1,28 +1,33 @@
-from flask import Flask, render_template, request, send_file, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory, make_response
 import librosa
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import matplotlib.textpath as textpath
 import matplotlib
-matplotlib.use('Agg')
 import os
 import uuid
-import textwrap
+import pickle
 
+matplotlib.use('Agg')
 
 app = Flask(__name__)
 
-# Set a directory to save generated images
+# Set directories to save generated images and DFT data
 UPLOAD_FOLDER = 'static/generated'
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
+DATA_FOLDER = 'static/data'
+for folder in [UPLOAD_FOLDER, DATA_FOLDER]:
+    if not os.path.exists(folder):
+        os.makedirs(folder)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DATA_FOLDER'] = DATA_FOLDER
+
 
 def load_audio(file):
     y, sr = librosa.load(file, sr=None)
     return y, sr
+
 
 def compute_dft_slices(y, sr, X_DIM, Y_DIM):
     n_slices = X_DIM * Y_DIM
@@ -31,10 +36,12 @@ def compute_dft_slices(y, sr, X_DIM, Y_DIM):
     dft_slices = [np.abs(np.fft.fft(slice)) for slice in slices]
     return dft_slices
 
+
 def create_colormap(start_hex, end_hex):
     start_rgb = mcolors.hex2color(start_hex)
     end_rgb = mcolors.hex2color(end_hex)
     return mcolors.LinearSegmentedColormap.from_list("custom_colormap", [start_rgb, end_rgb], N=256)
+
 
 def map_to_color(dft_slices, start_hex="#000000", end_hex="#FFFFFF"):
     max_frequencies = [np.argmax(slice) for slice in dft_slices]
@@ -42,6 +49,7 @@ def map_to_color(dft_slices, start_hex="#000000", end_hex="#FFFFFF"):
     colormap = create_colormap(start_hex, end_hex)
     colors = colormap(normalized_frequencies)
     return colors
+
 
 def reshape(colors, X_DIM, Y_DIM, Y_SCALE):
     image = np.reshape(colors, (Y_DIM, X_DIM, 4))[:, :, :3]
@@ -60,7 +68,7 @@ def add_text_to_image(ax, image_dim, text=None, color='white', text_width_percen
     """Adds specified text to the image at the center."""
     if text:
         target_width = text_width_percent / 100 * image_dim[1]
-        
+
         # Initial font size and width estimate
         fontsize = 1
         text_width = get_text_width(text, fontsize)
@@ -83,55 +91,89 @@ def add_text_to_image(ax, image_dim, text=None, color='white', text_width_percen
         ax.text(center_x, center_y, text, color=color, fontweight='bold',
                 fontsize=fontsize, ha='center', va='center')
 
+def generate_image_from_dft(dft_slices, X_DIM, Y_DIM, Y_SCALE, HEX_START, HEX_END):
+    colors = map_to_color(dft_slices, HEX_START, HEX_END)
+    image = reshape(colors, X_DIM, Y_DIM, Y_SCALE)
+    return image
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/process', methods=['POST'])
 def process_audio():
-    # Check if file was uploaded
+    print(request.form)
     if 'file' not in request.files:
         return 'No file part', 400
 
     MUSIC_FILE_NAME = request.files['file']
-
     if MUSIC_FILE_NAME.filename == '':
         return 'No selected file', 400
-    
+
+    X_DIM = 100
+    Y_SCALE = 2
+    Y_DIM = int(X_DIM//Y_SCALE)
+
+    y, sr = load_audio(MUSIC_FILE_NAME)
+    dft_slices = compute_dft_slices(y, sr, X_DIM, Y_DIM)
+
+    # Save DFT slices for later retrieval
+    data_filename = str(uuid.uuid4()) + ".pkl"
+    with open(os.path.join(app.config['DATA_FOLDER'], data_filename), 'wb') as f:
+        pickle.dump(dft_slices, f)
+
+    return jsonify({"data_filename": data_filename})
+
+
+@app.route('/generate_image', methods=['POST'])
+def generate_image():
+    data_filename = request.form['data_filename']
+    with open(os.path.join(app.config['DATA_FOLDER'], data_filename), 'rb') as f:
+        dft_slices = pickle.load(f)
+
     X_DIM = int(request.form['x_dim'])
     Y_SCALE = int(request.form['y_scale'])
     Y_DIM = int(X_DIM//Y_SCALE)
     HEX_START = request.form['hex_start']
     HEX_END = request.form['hex_end']
-    PDF_RESOLUTION = 300
-    text = request.form['text']
-    text_color = request.form['text_color']
-    text_width_percent = int(request.form['text_width_percent'])
+    ARTIST_NAME = request.form['artist_name']
+    SONG_TITLE = request.form['song_title']
+    TEXT_COLOR = request.form['text_color']
+    TEXT_WIDTH_PERCENT = int(request.form['text_width_percent'])
 
-    
-    y, sr = load_audio(MUSIC_FILE_NAME)
-    dft_slices = compute_dft_slices(y, sr, X_DIM, Y_DIM)
-    colors = map_to_color(dft_slices, HEX_START, HEX_END)
-    image = reshape(colors, X_DIM, Y_DIM, Y_SCALE)
-
+    image = generate_image_from_dft(dft_slices, X_DIM, Y_DIM, Y_SCALE, HEX_START, HEX_END)
     fig, ax = plt.subplots()
     ax.imshow(image)
     ax.axis('off')
 
     image_dim = image.shape
-    add_text_to_image(ax, image_dim, text, text_color, text_width_percent)
+    add_text_to_image(ax, image_dim, ARTIST_NAME, TEXT_COLOR, TEXT_WIDTH_PERCENT)
     # Turn off the axis
     plt.axis('off')
     # Ensure a tight layout
     plt.tight_layout()
-    # Save the generated image with a unique filename
-    filename = str(uuid.uuid4()) + ".png"
+
+    # Overwrite previous image or create new image file
+    filename = "generated.png"
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    plt.savefig(filepath,bbox_inches='tight', pad_inches=0, dpi=PDF_RESOLUTION)
+    plt.savefig(filepath, bbox_inches='tight', pad_inches=0)
     plt.close(fig)
 
-    # Return the generated image's URL
     return jsonify({"image_url": '/' + filepath})
+
+
+@app.route('/static/<path:path>')
+def send_static_file(path):
+    return send_from_directory('static', path)
+
+# Prevent caching of generated image
+@app.route('/static/generated/generated.png')
+def serve_image():
+    response = send_from_directory('static/generated', 'generated.png')
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
